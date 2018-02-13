7089511834ae1f71792a003faf915e29faf3ac08
jackmc <jackmc1122@gmail.com>
2018-02-13 10:17:16 -0500

    repplaced header files

--- fitness/about.php
+++ fitness/about.php
@@ -67,36 +67,7 @@
 		<div id="fh5co-wrapper">
 		<div id="fh5co-page">
 		<div id="fh5co-header">
-			<header id="fh5co-header-section">
-				<div class="container">
-					<div class="nav-header">
-						<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
-						<h1 id="fh5co-logo"><a href="index.html">Get<span>Fit</span></a></h1>
-						<!-- START #fh5co-menu-wrap -->
-						<nav id="fh5co-menu-wrap" role="navigation">
-							<ul class="sf-menu" id="fh5co-primary-menu">
-								<li class="active">
-									<a href="index.html">Home</a>
-								</li>
-								<li>
-									<a href="store.php" class="fh5co-sub-ddown">Store</a>
-									 <ul class="fh5co-sub-menu">
-									 	<li><a href="supplements.php">Supplements</a></li>
-									 	<li><a href="equipment.php">Equipment</a></li>
-									</ul>
-								</li>
-								<li>
-									<a href="schedule.html">Programs</a>
-								</li>
-								<li><a href="about.html">About</a></li>
-								<li><a href="contact.html">Contact</a></li>
-                                <li><a href="contact.html">Account</a></li>
-								<!-- <li><a href="#" data-toggle="modal" data-target="#loginModal">Login</a></li> -->
-							</ul>
-						</nav>
-					</div>
-				</div>
-			</header>		
+			<?php include 'header.php'; ?>		
 		</div>
 		<!-- end:fh5co-header -->
 		<div class="fh5co-parallax" style="background-image: url(images/home-image-4.jpg);" data-stellar-background-ratio="0.5">
