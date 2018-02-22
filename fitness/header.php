b11c3e05b017e21a7a19163f415d394929e8c337
JakeCornell <33403870+JakeCornell@users.noreply.github.com>
2018-02-22 10:31:54 -0500

    rearranged "about us" to be at the end of header.php

--- fitness/header.php
+++ fitness/header.php
@@ -19,9 +19,9 @@
                     <li>
                         <a href="programs.php">Programs</a>
                     </li>
-                    <li><a href="about.php">About</a></li>
                     <li><a href="contact.php">Contact</a></li>
                     <li><a href="account.php">Account</a></li>
+                    <li><a href="about.php">About</a></li>
                     <li><a href="account.php"><?php session_start(); echo $_SESSION['username'] ?></a></li>
                     <!-- <li><a href="#" data-toggle="modal" data-target="#loginModal">Login</a></li> -->
                 </ul>
